ed1379fe7440f4f04618dd6d7b43a012e5c5911d
patlaff <patlaff728@gmail.com>
2020-10-12 21:35:49 -0400

    init commit 2

---
+++ .github/build_procs.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+sql_command = '''
+create or replace procedure stproc1(FLOAT_PARAM1 FLOAT)
+    returns string
+    language javascript
+    strict
+    execute as owner
+    as
+    $$
+    var sql_command = 
+     "INSERT INTO stproc_test_table1 (num_col1) VALUES (" + FLOAT_PARAM1 + ")";
+    try {
+        snowflake.execute (
+            {sqlText: sql_command}
+            );
+        return "Succeeded.";   // Return a success/error indicator.
+        }
+    catch (err)  {
+        return "Failed: " + err;   // Return a success/error indicator.
+        }
+    $$
+    ;
+'''
