5d187bc09e3f0d072e84deff9cd4e58ef65e4bd6
Pat Laff <patlaff728@gmail.com>
2020-10-13 22:50:44 -0400

    working on tasks

---
+++ Stored-Procedures/sp_create_refined_pricesByCurrency.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE TABLE REFINED."pricesByCurrency" AS
+
+WITH pricesByProduct AS (
+  SELECT p."EnglishProductName", SUM(f."UnitPrice") as "UnitPriceSum", c."CurrencyAlternateKey"
+  FROM dbo."FactInternetSales" f
+  LEFT JOIN dbo."DimCurrency" c
+      ON f."CurrencyKey" = c."CurrencyKey"
+  LEFT JOIN dbo."DimProduct" p
+      ON f."ProductKey" = p."ProductKey"
+  GROUP BY p."EnglishProductName", c."CurrencyAlternateKey"
+)
+
+SELECT b."CurrencyName", SUM(a."UnitPriceSum") as "UnitPriceSum"
+FROM pricesByProduct a
+LEFT JOIN DBO."DimCurrency" b
+    ON a."CurrencyAlternateKey" = b."CurrencyAlternateKey"
+GROUP BY b."CurrencyName";
