69e49bd1622e975492ea444367619fc1946a71e7
Pat Laff <patlaff728@gmail.com>
2020-10-13 23:32:14 -0400

    added config file

--- .github/build_procs.py
+++ .github/build_procs.py
@@ -1,9 +1,11 @@
 import os
 import json
+import snowflake.connector
 
-directory = '../Stored-Procedures'
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+directory = os.path.dirname(__file__)+'/../Stored-Procedures'
 
-with open('config.json') as c:
+with open(os.path.join(__location__, 'config.json')) as c:
     config = json.load(c)
 
 tasks = []
@@ -12,24 +14,44 @@ for task in config['tasks']:
 
 for filename in os.listdir(directory):
 
-    taskname = ".".join(filename.split(".")[:-1]) 
+    taskname = ".".join(filename.split(".")[:-1])
+    warehouse = 'DEFAULT_WH'
+    schedule = None
+    dependency = None
+    comment = None
 
     with open(directory+'/'+filename) as file:
         sql_command = file.read()
 
     if taskname in tasks:
-        for 
+        for name, parameters in config.items():
+            if name==taskname:
+                if parameters['warehouse']:
+                    warehouse = parameters['warehouse']
+                    '''
+                    if ';' in warehouse:
+                        print('Invalid character (;) found in config file. Please revise.')
+                        sys.exit()
+                    '''
+                if parameters['schedule']:
+                    schedule = parameters['schedule']
+                if parameters['dependency']:
+                    dependency = parameters['dependency']
         warehouse = config['tasks']
-
+        
     task_script = f'''
     CREATE OR REPLACE TASK IF NOT EXISTS {taskname}
-    WAREHOUSE = 
-    SCHEDULE = '* * * * *'
     COPY GRANTS
-    COMMENT = '<string_literal>'
-    AFTER <string>
-    AS
-    ''' + sql_command
+    WAREHOUSE = '{warehouse}'
+    '''
+    if schedule: # or schedule <> '':
+        task_script += f"\nSCHEDULE = '{schedule}'"
+    if comment:
+        task_script += f"\nCOMMENT = '{comment}'"
+    if dependency:
+        task_script += f"\nAFTER {dependency}"
+    
+    task_script += f'AS\n{sql_command}'
 
-    #print(task_script)
+    print(task_script)
     
